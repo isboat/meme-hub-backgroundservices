b0ae58713381a62c3cf905cde36c61949c4dc05c
isaac boateng <isboateng@hotmail.com>
2024-12-19 10:47:21 +0000

    init proj

---
+++ src/Meme.Hub.BackgroundServices/Meme.Hub.PumpFunListener/Program.cs
@@ -0,0 +1,32 @@
+using Meme.Hub.PumpFunListener.Services;
+
+namespace Meme.Hub.PumpFunListener
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            // Register Hosted Services
+            builder.Services.AddHostedService<BackgroundListener>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ src/Meme.Hub.BackgroundServices/Meme.Hub.PumpFunListener/Services/BackgroundListener.cs
@@ -0,0 +1,54 @@
+﻿
+using Meme.Domain.Models.Settings;
+using Microsoft.Extensions.Options;
+using System.Runtime;
+
+namespace Meme.Hub.PumpFunListener.Services
+{
+    public class BackgroundListener : BackgroundService
+    {
+        private readonly ILogger<BackgroundListener> _logger;
+        private readonly SolanaDataSourceSettings _solanaDataSourceSettings;
+        private readonly MessagingBusSettings _messagingBusSettings;
+
+        //private readonly IEventBus _eventBus;
+
+        public BackgroundListener(IOptions<SolanaDataSourceSettings> solanaDataSourceSettings,
+            IOptions<MessagingBusSettings> messagingBusSettings,
+                                     //IEventBus eventBus,
+                                     ILogger<BackgroundListener> logger)
+        {
+            _logger = logger;
+            _solanaDataSourceSettings = solanaDataSourceSettings.Value;
+            _messagingBusSettings = messagingBusSettings.Value;
+        }
+
+        protected async override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //_logger.LogDebug($"GracePeriodManagerService is starting.");
+
+            //stoppingToken.Register(() =>_logger.LogDebug($" GracePeriod background task is stopping."));
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                Console.WriteLine("backgroun service is runing");
+                //_logger.LogDebug($"GracePeriod task doing background work.");
+
+                // This eShopOnContainers method is querying a database table
+                // and publishing events into the Event Bus (RabbitMQ / ServiceBus)
+                //CheckConfirmedGracePeriodOrders();
+
+                try
+                {
+                   await Task.Delay(500, stoppingToken);
+                }
+                catch (TaskCanceledException exception)
+                {
+                    //_logger.LogCritical(exception, "TaskCanceledException Error", exception.Message);
+                }
+            }
+
+            //_logger.LogDebug($"GracePeriod background task is stopping.");
+        }
+    }
+}
